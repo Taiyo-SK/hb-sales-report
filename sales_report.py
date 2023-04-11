6e889f062ad4ffa813a2ac48b9a2c0ebf17d8308
Taiyo Scanlon-Kimura <taiyo.scanlon-kimura@fulbrightmail.org>
2023-04-11 16:24:12 -0700

    attempted to make a sales report with melons sold and total dollars netted per salesperson--unsuccessful

--- sales_report.py
+++ sales_report.py
@@ -1,35 +1,84 @@
 """Generate sales report showing total melons each salesperson sold."""
 
+# comments explaining original code marked with one hashtag #
+## areas for improvement marked with two hashtags ##
 
-salespeople = [] # creates empty list for salespeople
-melons_sold = [] # creates empty list for melons sold
-# guessing that both of these are outputted at the end of the program
+## instead of global lists, could we create a sales order dictionary?
+## that way, we don't have to check, append the global lists by each line of file
+ 
+def sales_order_dict(log_file_path):
+    """Return a dictionary of {salesperson_name: total_dollars, melons_sold}.
+    Arguments:
+        log_file_path (str) - the path to a sales report log file
+    
+    Return:
+        melons_by_sales (dict)
+    """
 
-f = open('sales-report.txt') # opens the sales report file, i.e. our sales data
-# this is our data variable for the program
+    melons_by_sales = {}
 
-for line in f: # begins a for loop to iterate over each line in our data
-    line = line.rstrip() # removes trailing space and the line break indicators
-    entries = line.split('|') # yields the three pieces of data in each line, as separate strings
-                                # returns as a list of strings
-    salesperson = entries[0] # var where the first element of the list is the salesperson's name
-    melons = int(entries[2]) # var where the third element is the number of melons
-                                # type cast as integer and assign to var
+    with open(log_file_path) as f:
+        for line in f: # begins a for loop to iterate over each line in our data
+            line = line.rstrip() # removes trailing space and the line break indicators
 
-    if salesperson in salespeople: # begins a nested if statement inside our for loop
-                                    # compares the specific salesperson 
-                                    # in the global salespeople list
-        position = salespeople.index(salesperson) # assigns the index position for 
-                                                    # that salesperson in the global list
-        melons_sold[position] += melons # adds the number of melons from line 16 
-                                        # to the global melons_sold list
-        
-    else:
-        salespeople.append(salesperson) # add the salesperson from the line 
-                                        #to the global salespeople list
-        melons_sold.append(melons)  # add the melons sold from the line 
-                                    # to the global melons sold list
+        ## create a list of data and unpack its values
+        salesperson_name, total_dollars, melons_sold = line.split('|')
+        if salesperson_name in melons_by_sales:
+            melons_by_sales[salesperson_name[0]] += float(total_dollars)
+            melons_by_sales[salesperson_name[1]] += int(melons_sold)
+        else:
+                melons_by_sales[salesperson_name[0]] = float(total_dollars)
+                melons_by_sales[salesperson_name[1]] = int(melons_sold)
+
+    return melons_by_sales
+
+def print_sales_report(sales_order_dict):
+     """Print a report of salespeople, their total number of melons sold, 
+        and their total sales.
+    Arguments: sales_order_dict - {salesperson_name: total_dollars, melons_sold}.
+     """
+for sales_person_name, total_dollars, melons_sold in sales_order_dict.items():
+    print(f"{sales_person_name} sold {melons_sold} melons netting {total_dollars} in sales.")
+
+print_sales_report(sales_order_dict('sales-report.txt'))
+
+# def get_melons_sold_by_salesperson(log_file_path):
+#     """Return a dictionary of {salesperson_name: melons_sold}.
+
+#     Arguments:
+#         log_file_path (str) - the path to a sales report log file
+
+#     Return:
+#         mels_by_sales (dict)
+#     """
+
+#     mels_by_sales = {}
+
+#     with open(log_file_path) as f:
+#         for line in f:
+#             line = line.rstrip()
+
+#             # Create a list of data and unpack its values
+#             salesperson_name, total_dollars, melons_sold = line.split('|')
+
+#             # Set or increment the salesperson's total melons sold
+#             if salesperson_name in mels_by_sales:
+#                 mels_by_sales[salesperson_name] += int(melons_sold)
+#             else:
+#                 mels_by_sales[salesperson_name] = int(melons_sold)
+
+#     return mels_by_sales
 
 
-for i in range(len(salespeople)): # make a range of the salespeople list and iterate
-    print(f'{salespeople[i]} sold {melons_sold[i]} melons') # the sales report of melons per salesperson
+# def print_sales_report(melons_sold_by_salesperson):
+#     """Print a report of salespeople and the total number of melons they've sold.
+
+#     Arguments:
+#         melons_sold_by_salesperson (dict) - {salesperson_name: melons_sold}
+#     """
+
+#     for salesperson_name, melons_sold in melons_sold_by_salesperson.items():
+#         print(f'{salesperson_name} sold {melons_sold} melons')
+
+
+# print_sales_report(get_melons_sold_by_salesperson('sales-report.txt'))
